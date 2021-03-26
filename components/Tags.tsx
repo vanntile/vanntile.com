870c0c98b00f1118dbc689a0a5a1e0d3c2ccc98a
vanntile <33692650+vanntile@users.noreply.github.com>
2021-03-26 18:23:07 +0200

    fix: :bug: missing tags keys

--- components/Tags.tsx
+++ components/Tags.tsx
@@ -7,7 +7,9 @@ interface Props {
 const Tags: React.FC<Props> = ({ tags, additionalClasses, className }): JSX.Element => (
   <div className={`pb-4 w-full overflow-hidden flex flex-row flex-wrap gap-2 ${className}`}>
     {tags.map((s: string) => (
-      <span className={`px-4 py-2 whitespace-nowrap text-sm font-semibold rounded ${additionalClasses}`}>{s}</span>
+      <span key={s} className={`px-4 py-2 whitespace-nowrap text-sm font-semibold rounded ${additionalClasses}`}>
+        {s}
+      </span>
     ))}
   </div>
 )
