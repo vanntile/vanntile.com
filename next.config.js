a751c5b386f3da8378080dae84bf8b73d1367562
vanntile <33692650+vanntile@users.noreply.github.com>
2021-08-13 22:37:45 +0300

    chore(security): :nut_and_bolt: update security settings

--- next.config.js
+++ next.config.js
@@ -26,21 +26,40 @@ module.exports = {
               styleSrc: ["'self'", "'unsafe-inline'", isProd ? null : "'unsafe-eval'"],
               fontSrc: "'self'",
               objectSrc: "'none'",
+              mediaSrc: "'none'",
               frameAncestors: "'none'",
               formAction: "'none'",
+              workerSrc: "'none'",
               baseURI: "'self'",
             },
           },
-          forceHTTPSRedirect: [true, { maxAge: 60 * 60 * 24 * 30 * 6, includeSubDomains: true, preload: true }],
+          forceHTTPSRedirect: [true, { maxAge: 60 * 60 * 24 * 365, includeSubDomains: true, preload: true }],
           referrerPolicy: 'same-origin',
         }),
       },
       {
         source: '/(.*)',
         headers: [
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+          // Opt-out of Google FLoC: https://amifloced.org/
           {
             key: 'Permissions-Policy',
-            value: 'camera=(), microphone=(), geolocation=()',
+            value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
           },
         ],
       },
