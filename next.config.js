05f7f9c3d318c3bd2e15fb5e376124a4ff5a3b7c
vanntile <33692650+vanntile@users.noreply.github.com>
2021-08-13 18:28:52 +0300

    chore(deps): :nut_and_bolt: update unified extensions
    and remove top level mdx pages

--- next.config.js
+++ next.config.js
@@ -1,27 +1,14 @@
-const mdx = require('@next/mdx')
-const mdxPrism = require('mdx-prism')
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 const { createSecureHeaders } = require('next-secure-headers')
-const reHeadings = require('remark-autolink-headings')
-const reCodeTitles = require('remark-code-titles')
-const reHint = require('remark-hint')
-const reHtml = require('remark-html')
-const reSlug = require('remark-slug')
-const reTOC = require('remark-toc')
 
 const isProd = process.env.NODE_ENV === 'production'
 
-const withMDX = mdx({
-  options: {
-    remarkPlugins: [reTOC, reCodeTitles, reSlug, reHeadings, reHint, reHtml],
-    rehypePlugins: [mdxPrism],
-  },
-})
-
 // TODO: HTTPS redirect
 
-module.exports = withMDX({
-  pageExtensions: ['mdx', 'ts', 'tsx'],
+module.exports = {
+  experimental: {
+    esmExternals: true,
+  },
+  pageExtensions: ['mdx', 'tsx'],
   poweredByHeader: false,
   async headers() {
     return [
@@ -59,4 +46,4 @@ module.exports = withMDX({
       },
     ]
   },
-})
+}
