60c392e7780ef1fc7cb9177c373be4608cc3eb61
vanntile <33692650+vanntile@users.noreply.github.com>
2023-02-22 22:36:22 +0200

    wip(deps): :construction: update deps
    - upgrade astro
    - go from prism to shiki
    - use rehype-pretty-code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,52 +1,68 @@
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
-import remarkAutolinkHeadings from 'rehype-autolink-headings'
-import remarkCodeTitles from 'remark-code-titles'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypePrettyCode from 'rehype-pretty-code'
+import rehypeSlug from 'rehype-slug'
+import rehypeSortAttributes from 'rehype-sort-attributes'
+import rehypeToc from 'rehype-toc'
 import remarkHint from 'remark-hint'
-import remarkHtml from 'remark-html'
-import remarkSlug from 'remark-slug'
-import remarkToc from 'remark-toc'
 import { remarkReadingTime } from './src/lib/plugins.mjs'
+
+// https://rehype-pretty-code.netlify.app/
+const prettyCodeOptions = {
+  theme: 'one-dark-pro',
+  keepBackground: false,
+
+  onVisitLine(node) {
+    // Prevent lines from collapsing in `display: grid` mode, and allow empty lines to be copy/pasted
+    if (node.children.length === 0) {
+      node.children = [{ type: 'text', value: ' ' }]
+    }
+  },
+
+  onVisitHighlightedLine(node) {
+    node.properties.className.push('highlighted')
+  },
+
+  onVisitHighlightedWord(node) {
+    node.properties.className = ['word']
+  },
+}
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://vanntile.com',
   markdown: {
-    syntaxHighlight: 'prism',
-  },
-  integrations: [
-    tailwind({ config: { applyBaseStyles: false } }),
-    mdx({
-      remarkPlugins: {
-        extends: [
-          [
-            remarkToc,
-            {
-              tight: true,
-              ordered: true,
-            },
-          ],
-          remarkCodeTitles,
-          remarkSlug,
-          remarkHint,
-          remarkHtml,
-          remarkReadingTime,
-        ],
-      },
-      rehypePlugins: [
-        [
-          remarkAutolinkHeadings,
-          {
-            behavior: 'prepend',
-            properties: {
-              ariaLabel: 'Anchor',
-              tabIndex: -1,
-              className: ['icon-link'],
-            },
+    syntaxHighlight: false,
+    remarkPlugins: [remarkHint, remarkReadingTime],
+    rehypePlugins: [
+      [rehypePrettyCode, prettyCodeOptions],
+      [
+        rehypeToc,
+        {
+          nav: false,
+          cssClasses: {
+            list: '',
+            listItem: '',
+            link: '',
           },
-        ],
+        },
       ],
-    }),
-  ],
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'prepend',
+          properties: {
+            ariaLabel: 'Anchor',
+            tabIndex: -1,
+            className: ['icon-link'],
+          },
+        },
+      ],
+      rehypeSortAttributes,
+    ],
+  },
+  integrations: [tailwind({ config: { applyBaseStyles: false } }), mdx()],
 })
