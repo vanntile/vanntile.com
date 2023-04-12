4e938052471a54654ffb778be84f72ac2bd67237
vanntile <33692650+vanntile@users.noreply.github.com>
2023-04-12 14:07:27 +0300

    fix(seo): :bug: update seo tags and social media links

---
+++ src/lib/social.js
@@ -0,0 +1,36 @@
+import svg from './svgPaths.json'
+
+const socialMedia = [
+  {
+    href: 'https://github.com/vanntile',
+    name: 'vanntile on GitHub',
+    d: svg.github,
+  },
+  {
+    href: 'https://www.linkedin.com/in/valentin-ionita',
+    name: 'vanntile on LinkedIn',
+    d: svg.linkedin,
+  },
+  {
+    href: 'https://fosstodon.org/@vanntile',
+    name: 'vanntile on Mastodon',
+    d: svg.mastodon,
+  },
+  {
+    href: 'https://stackoverflow.com/users/4679160/vanntile-ianito',
+    name: 'vanntile on StackOverflow',
+    d: svg.stackoverflow,
+  },
+  {
+    href: 'https://gitlab.com/vanntile',
+    name: 'vanntile on GitLab',
+    d: svg.gitlab,
+  },
+  {
+    href: 'https://dribbble.com/vanntile',
+    name: 'vanntile on Dribbble',
+    d: svg.dribbble,
+  },
+]
+
+export default socialMedia
